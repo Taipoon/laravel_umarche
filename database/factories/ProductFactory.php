f4fb252350f31866c215ebc8aa60dcea7e8139f1
Taipoon <taiki.hirayama.kpu-gmail.com>
2021-12-28 16:36:00 +0900

    表示件数

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -25,14 +25,15 @@ class ProductFactory extends Factory
             'name' => $this->faker->name,
             'information' => $this->faker->realText,
             'price' => $this->faker->numberBetween(10, 100000),
-            'is_selling' => $this->faker->numberBetween(0,1),
+            // 'is_selling' => $this->faker->numberBetween(0,1),
+            'is_selling' => 1,
             'sort_order' => $this->faker->randomNumber,
-            'shop_id' => $this->faker->numberBetween(1,2),
-            'secondary_category_id' => $this->faker->numberBetween(1,6),
-            'image1' => $this->faker->numberBetween(1,6),
-            'image2' => $this->faker->numberBetween(1,6),
-            'image3' => $this->faker->numberBetween(1,6),
-            'image4' => $this->faker->numberBetween(1,6),
+            'shop_id' => $this->faker->numberBetween(1, 2),
+            'secondary_category_id' => $this->faker->numberBetween(1, 6),
+            'image1' => $this->faker->numberBetween(1, 6),
+            'image2' => $this->faker->numberBetween(1, 6),
+            'image3' => $this->faker->numberBetween(1, 6),
+            'image4' => $this->faker->numberBetween(1, 6),
 
         ];
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,7 +26,7 @@ class DatabaseSeeder extends Seeder
             // StockSeeder::class,
             UserSeeder::class
         ]);
-        Product::factory(100)->create();
+        Product::factory(300)->create();
         Stock::factory(100)->create();
     }
 }
